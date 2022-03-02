8df4fd4597ca19b0209f18a5aa47a677a0d33d6d
ramanvishal <100151999+ramanvishal@users.noreply.github.com>
2022-03-02 14:44:21 +0530

    Create odd even .js

---
+++ odd even .js
@@ -0,0 +1,11 @@
+const number =("Enter a number: ");
+if(number % 2 == 0 && num % 3==0) {
+    console.log("The number is even odd.");
+}
+
+else if (num % 2==0){
+    console.log("The number is even.");
+}
+else  (num % 3==0){
+    console.log("The number is odd.");
+}
