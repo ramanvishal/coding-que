6708e233eac89b8ed1ae4622d6eaf04133f32676
ramanvishal <100151999+ramanvishal@users.noreply.github.com>
2022-03-02 14:29:15 +0530

    Update add.js

--- add.js
+++ add.js
@@ -1,6 +1,4 @@
 const num1 = ('Enter the first number '));
 const num2 = ('Enter the second number '));
 const sum = num1 + num2;
-
-// display the sum
 console.log(`The sum of ${num1} and ${num2} is ${sum}`);
